6997bd0c4ae96c38ee75bda3abc54477b5a2eebb
vkirichenko <vitaliy.v.kirichenko@gmail.com>
2018-11-19 13:18:49 +0200

    Merge remote-tracking branch 'remotes/origin/feature/lep-listener' into feature/permissions
    # Conflicts:
    #	src/main/java/com/icthh/xm/tmf/ms/account/service/PartyAccountDelegate.java

--- src/main/java/com/icthh/xm/tmf/ms/account/service/PartyAccountDelegate.java
+++ src/main/java/com/icthh/xm/tmf/ms/account/service/PartyAccountDelegate.java
@@ -5,15 +5,11 @@ import com.icthh.xm.commons.lep.LogicExtensionPoint;
 import com.icthh.xm.commons.lep.spring.LepService;
 
 import com.icthh.xm.tmf.ms.account.api.PartyAccountApiDelegate;
-import com.icthh.xm.tmf.ms.account.model.EntitySpecification;
 import com.icthh.xm.tmf.ms.account.model.PartyAccount;
-import com.icthh.xm.tmf.ms.account.model.SpecCharacteristic;
-import com.icthh.xm.tmf.ms.account.model.SpecCharacteristicValue;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
-import java.time.OffsetDateTime;
 import java.util.Collections;
 import java.util.List;
 
@@ -21,28 +17,10 @@ import java.util.List;
 @LepService(group = "service", name = "default")
 public class PartyAccountDelegate implements PartyAccountApiDelegate {
 
-    //@LogicExtensionPoint("RetrievePartyAccount")
+    @LogicExtensionPoint("RetrievePartyAccount")
     @PreAuthorize("hasPermission({'id': #id}, 'PARTY.ACCOUNT.GET')")
     @Override
-    public ResponseEntity<List<PartyAccount>> retrievePartyAccount(String id) {
-        CustomPartyAccount partyAccount = new CustomPartyAccount();
-
-        partyAccount.setBaseType(PartyAccount.class.getSimpleName());
-        partyAccount.setType(CustomPartyAccount.class.getSimpleName());
-        partyAccount.setSchemaLocation("http://account/schema/CustomPartyAccount.yml");
-
-
-        EntitySpecification specification = new EntitySpecification().name("TestSpec")
-            .addSpecCharacteristicItem(
-                new SpecCharacteristic().name("TestChar").addSpecCharacteristicValueItem(
-                    new SpecCharacteristicValue().value("dsdfsdf")
-                ));
-
-
-        partyAccount.setSpecification(specification);
-        partyAccount.setId("1232");
-        partyAccount.setLastModified(OffsetDateTime.now());
-
-        return ResponseEntity.ok(Collections.singletonList(partyAccount));
+    public ResponseEntity<List<PartyAccount>> retrievePartyAccount(String id, String profile) {
+        return ResponseEntity.ok(Collections.emptyList());
     }
 }
--- src/main/java/com/icthh/xm/tmf/ms/account/service/XmPartyAccount.java
+++ src/main/java/com/icthh/xm/tmf/ms/account/service/XmPartyAccount.java
@@ -5,12 +5,9 @@ import com.icthh.xm.tmf.ms.account.model.PartyAccount;
 import lombok.Getter;
 import lombok.Setter;
 
-/**
- * @author Vitaliy Kirichenko (vitaliy.v.kirichenko@gmail.com)
- */
 @Getter
 @Setter
-public class CustomPartyAccount extends PartyAccount {
+public class XmPartyAccount extends PartyAccount {
 
     private EntitySpecification specification;
 }
